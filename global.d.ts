cf17c1589ccaafdade223f255c4dc9f96bfd9b32
Saúl Páez <saulpaez03@gmail.com>
2023-04-23 15:00:00 -0400

    Basic layout and mint button

---
+++ global.d.ts
@@ -0,0 +1,7 @@
+import {} from "ethers";
+import { Eip1193Provider } from "ethers/types/providers";
+declare global {
+  interface Window {
+    ethereum: Eip1193Provider;
+  }
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>MyToken - Mint</title>
+      </Head>
+
+      <ThemeProvider theme={createTheme()}>
+        <Component {...pageProps} />
+        <ToastContainer
+          position="bottom-center"
+          autoClose={false}
+          rtl={false}
+          closeOnClick={false}
+          pauseOnFocusLoss
+          pauseOnHover
+          theme="light"
+        ></ToastContainer>
+      </ThemeProvider>
+    </>
+  );
 }
